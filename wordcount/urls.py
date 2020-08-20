dbba3a5cf2103450f7b03a729d254f97c1e361f7
jokery321 <jonathankery@gmail.com>
2020-08-21 09:31:01 +1000

    added a comment

--- wordcount/urls.py
+++ wordcount/urls.py
@@ -18,8 +18,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('eggs/', views.eggs),
-    path('about/', views.users)
+    path('', views.homepage),
+    path('count/', views.count, name='count')
 
 ]
--- wordcount/views.py
+++ wordcount/views.py
@@ -3,13 +3,21 @@ from django.shortcuts import render
 
 # here are the functions that send you to the url you've specified
 
-def home(request):
+
+def homepage(request):
     return render(request, 'home.html')
 
 
-def about(request):
-    return HttpResponse('Eggs are great!')
+def count(request):
+    fulltext = request.GET['fulltext']
+    word_list = fulltext.split()
+    word_dict = {}
+    for word in word_list:
+        if word in word_dict:
+            word_dict[word] += 1
+        else:
+            word_dict[word] = 1
 
+    return render(request, 'count.html', {'fulltext':fulltext,
+                    'count':len(word_list), 'word_dict':word_dict.items()})
 
-def users(request):
-    return render(request, 'users.html')
